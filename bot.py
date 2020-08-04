a72c454c624f3beca0499c2eab511cee5f7bc6ed
Sergey-ops <62600934+Sergey-ops@users.noreply.github.com>
2020-08-04 06:30:38 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import os
 import discord
 from discord.ext import commands
 token = 'токен' 
-words = ["маты", "маты", "маты"] 
+words = ["хуй", "маты", "маты"] 
 bot = commands.Bot(command_prefix= 'префикс')
 @bot.event
 async def on_message(message) :
